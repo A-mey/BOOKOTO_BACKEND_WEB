4959072b4c5da13db2ca6a65b3f40e5fe1ed5dbe
Ameya <Ameya.Patil@playerzpot.com>
2023-12-14 14:58:29 +0530

    changes

--- src/startup/models/data/user.data.models.ts
+++ src/startup/models/data/user.data.models.ts
@@ -1,5 +1,5 @@
 import { IUserInterface } from "../../interfaces/IUser.interface";
 
 export abstract class User implements IUserInterface{
-    abstract processSession (): Promise<{SESSION_ID: string; data: object;}> | Promise<{SESSION_ID: string}>
+    abstract processSession (): Promise<{SESSION_ID: string; data: object;} | {SESSION_ID: string}>
 }
