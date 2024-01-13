d5c81037d36051c3c043b5fc2063fc01d4e2e58c
Ameya <Ameya.Patil@playerzpot.com>
2024-01-14 03:21:02 +0530

    changes

--- src/common/session/interfaces/ISession.service.interface.ts
+++ src/common/session/interfaces/ISession.service.interface.ts
@@ -1,11 +1,11 @@
-import { response } from "../../types/response.types";
+import { Response } from "../../types/response.types";
 
 export interface ISessionServiceInterface {
     validateSession (userId: string, sessionId: string) : Promise<boolean>
 
     createSession () : Promise<{SESSION_ID: string}>
 
-    getSessionData (sessionId: string) : Promise<response>
+    getSessionData (sessionId: string) : Promise<Response>
 
     addSession (addSessionDto: {USERDATA: object, SET: string}) : Promise<void>
 }
