2d1216d42bb572bf76cf6f6039cb97c607a0d8f0
Ameya <Ameya.Patil@playerzpot.com>
2024-01-16 19:59:44 +0530

    changes

--- src/product/dao/product.dao.ts
+++ src/product/dao/product.dao.ts
@@ -1,3 +1,4 @@
+import { catchError } from "../../common/utils/catch.util";
 import { Product } from "../types/product.type"
 
 export class ProductDao {
@@ -6,10 +7,11 @@ export class ProductDao {
 
     getAllProductsDao = async (from: number, to: number) : Promise<Product> => {
         try {
-            const productList: Product = [];
-            const products = 
+            const products = ProductList.splice(from,to);
+            return products;
         } catch (error: unknown) {
-
+            const errorMsg = await catchError(error);
+            throw new Error(errorMsg);
         }
     }
 }
