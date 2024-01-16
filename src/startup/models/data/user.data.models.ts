9de094536c4b10ce3592b56f741f372f10bd240c
Ameya <Ameya.Patil@playerzpot.com>
2024-01-16 12:56:00 +0530

    changes

--- src/startup/models/data/user.data.models.ts
+++ src/startup/models/data/user.data.models.ts
@@ -1,18 +1,12 @@
 import { SessionService } from "../../../common/session/services/session.service";
 import { IUserInterface } from "../../interfaces/IUser.interface";
-import { SessionDao } from "../../../common/session/dao/session.dao";
 import { ISessionDaoInterface } from "../../../common/session/interfaces/ISession.dao.interface";
-import { SessionDaoTest } from "../../../test/dao/session.dao.test";
+import { SessionDaoFactory } from "../../../container/daoFactory/session.dao.factory";
 
 export abstract class User implements IUserInterface{
     sessionService: SessionService;
     constructor() {
-        let sessionDao: ISessionDaoInterface;
-        if (process.env.DEPLOY_STAGE === "qc") {
-            sessionDao = new SessionDaoTest();
-        } else {
-            sessionDao = new SessionDao();
-        }
+        const sessionDao: ISessionDaoInterface = new SessionDaoFactory().getDao();
         this.sessionService = new SessionService(sessionDao);
     }
     abstract processSession (): Promise<{SESSION_ID: string; data: object;} | {SESSION_ID: string}>
