4dca7f7181f745981b3a0dffc47e99dc4b0e7269
Ameya <Ameya.Patil@playerzpot.com>
2024-02-01 16:39:06 +0530

    changes

--- src/container/routes/startup/startup.container.ts
+++ src/container/routes/startup/startup.container.ts
@@ -5,10 +5,14 @@ import { StartupService } from "../../../startup/services/startup.service";
 import { sessionContainer } from "../../session/session.container";
 import { ProductDaoFactory } from "../../daoFactory/product.dao.factory";
 import { ProductService } from "../../../product/services/product.service";
+import { RecentService } from "../../../recent/services/recent.service";
+import { RecentDAO } from "../../../recent/dao/recent.dao";
 
 export const startupContainerService = (app: express.Application) : StartupRoutes => {
     const productDao = new ProductDaoFactory().getDao();
-    const productService = new ProductService(productDao);
+    const recentDao = new RecentDAO();
+    const recentService = new RecentService(recentDao);
+    const productService = new ProductService(productDao, recentService);
     const sessionService = sessionContainer();
     const startupService = new StartupService(sessionService, productService);
     const startupController = new StartupController(startupService);
