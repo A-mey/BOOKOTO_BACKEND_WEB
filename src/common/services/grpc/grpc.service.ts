4a3553a6c3fabf96bea3b003027544ce48a9d56b
Ameya <amey2p@gmail.com>
2025-04-01 08:03:41 +0530

    created grpc

--- src/common/services/grpc/grpc.service.ts
+++ src/common/services/grpc/grpc.service.ts
@@ -1,19 +1,21 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
-import path from "path";
+// import path from "path";
 
 export default class GrpcService<T> {
     private client: T;
 
-    constructor(protoPath: string, serviceName: string, address: string) {
-        const packageDefinition = protoLoader.loadSync(path.resolve(__dirname, protoPath));
+    constructor(protoPath: string, packageName: string, serviceName: string, address: string) {
+        const packageDefinition = protoLoader.loadSync(protoPath);
         const grpcObject = grpc.loadPackageDefinition(packageDefinition) as Record<string, unknown>;
-
+        console.log("Loaded gRPC Services:", Object.keys(grpcObject));
         if (!grpcObject[serviceName]) {
             throw new Error(`Service ${serviceName} not found in proto file.`);
         }
 
-        this.client = new (grpcObject[serviceName] as { new (...args: unknown[]): T })(
+        const newPackage = grpcObject[packageName] as Record<string, unknown>;
+
+        this.client = new (newPackage[serviceName] as { new (...args: unknown[]): T })(
             address,
             grpc.credentials.createInsecure()
         );
--- src/login/dao/login.grpc.dao.ts
+++ src/login/dao/login.grpc.dao.ts
@@ -5,22 +5,50 @@ import { LoginResponseType } from "../types/login.response.type";
 // import { ValidateOtpDTO } from "../dto/validate.otp.dto";
 // import { LoginUserDTO } from "../dto/login.user.dto";
 // import { RegisterUserDTO } from "../dto/register.user.dto";
-import GrpcClient from "../../common/services/grpc/grpc.service";
+// import GrpcClient from "../../common/services/grpc/grpc.service";
 import ILoginGrpcDaoInterface from "../interfaces/ILogin.grpc.dao.interface";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import path from "path";
 
 
 export default class LoginGrpcDao {
     client: ILoginGrpcDaoInterface;
 
-    constructor () {
-        const grpcClient = new GrpcClient<ILoginGrpcDaoInterface>("../protos/login.proto", "login.LoginService", "localhost:50051");
-        this.client = grpcClient.getClient();
-    }
+    // constructor () {
+    //     console.log("path", path.resolve(__dirname, "../protos/login.proto"));
+    //     const grpcClient = new GrpcClient<ILoginGrpcDaoInterface>(path.resolve(__dirname, "../protos/login.proto"), "login", "LoginService", "localhost:2004");
+    //     this.client = grpcClient.getClient();
+    // }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // createOTPDao = async (_createOtpDTO: CreateOtpDTO): Promise<Response> => {
-    //     return {success: true, code: 200, data: {message: "OTP sent", data: {fullHash: "12345"}}};
-    // };
+    constructor() {
+        // Load the .proto file
+        const protoPath = path.resolve(__dirname, "../protos/login.proto");
+        const packageDefinition = protoLoader.loadSync(protoPath, {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true,
+        });
+
+        // Load the gRPC object
+        const grpcObject = grpc.loadPackageDefinition(packageDefinition) as Record<string, unknown>;
+        console.log("Loaded gRPC Services:", Object.keys(grpcObject)); // Debugging output
+
+        // Get the package where the service is located
+        const packageObj = grpcObject["login"] as Record<string, unknown>;
+
+        if (!packageObj || !packageObj["LoginService"]) {
+            throw new Error("Service LoginService not found in proto file.");
+        }
+
+        // Instantiate the gRPC client
+        this.client = new (packageObj["LoginService"] as { new (...args: unknown[]): ILoginGrpcDaoInterface })(
+            "localhost:2004",
+            grpc.credentials.createInsecure()
+        );
+    }
 
     loginUser = async (email: string, password: string): Promise<LoginResponseType> => {
         return new Promise((resolve, reject) => {
